974e0e54c62791c7b2274965ad2ec942bb2d4aea
andys <andysmith836@gmail.com>
2019-09-09 22:58:23 +0100

    Add emoji art

---
+++ EmojiArt/EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Andy Smith on 9/4/19.
+//  Copyright © 2019 Andy Smith. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+
+  @IBOutlet weak var label: UILabel!
+  
+}
